38db6cb7e2b8540b23db915740e78ba9a6ba7a04
Kevin O Keeffe <kevokeeffe@gmail.com>
2024-02-13 20:49:48 +0000

    updated seo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,37 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'Adams Puppies';
+  constructor(private meta: Meta, private title: Title) {
+    this.title.setTitle('Furs Things First');
+    this.meta.addTags([
+      {
+        name: 'description',
+        content:
+          'Furs Things First is a professional dog training business in Waterford, Ireland. We offer a variety of services and techniques to help you and your furry friend.',
+      },
+      {
+        name: 'keywords',
+        content:
+          'dog, training, waterford, ireland, furs things first, obedience, agility, behavior, puppy, clicker, positive, reinforcement, socialization, crate, leash, manners, tricks, fun, games',
+      },
+      { name: 'author', content: 'Furs Things First' },
+      { rel: 'canonical', href: 'https://fursthingsfirst.ie' },
+      {
+        name: 'og:title',
+        content: 'Furs Things First - Dog Training in Waterford',
+      },
+      {
+        name: 'og:description',
+        content:
+          'Furs Things First is a professional dog training business in Waterford, Ireland. We offer a variety of services and techniques to help you and your furry friend.',
+      },
+      { name: 'og:image', content: 'https://fursthingsfirst.ie/logo.png' },
+      { name: 'og:url', content: 'https://fursthingsfirst.ie' },
+      { name: 'og:type', content: 'website' },
+      { name: 'og:locale', content: 'en_IE' },
+      { name: 'fb:app_id', content: '61556219123886' },
+      { name: 'fb:pages', content: '61556219123886' },
+      { name: 'instagram:creator', content: '@furs_things_first' },
+    ]);
+  }
 }
