e4ee23416a637d8e566e700948c9aaed0cf65fb5
Kevin O Keeffe <kevokeeffe@gmail.com>
2024-02-15 02:36:36 +0000

    mobile detection

--- src/app/layout/bottom-sheets/contact-bottom-sheet/contact-bottom-sheet.component.ts
+++ src/app/layout/bottom-sheets/contact-bottom-sheet/contact-bottom-sheet.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { MatBottomSheetRef } from '@angular/material/bottom-sheet';
-
+import { DeviceDetectorService } from 'ngx-device-detector';
 @Component({
   selector: 'app-contact-bottom-sheet',
   templateUrl: './contact-bottom-sheet.component.html',
   styleUrl: './contact-bottom-sheet.component.scss',
 })
 export class ContactBottomSheetComponent {
+  phoneNumber: string = '+353 85 137 4535';
   constructor(
-    private _bottomSheetRef: MatBottomSheetRef<ContactBottomSheetComponent>
+    private _bottomSheetRef: MatBottomSheetRef<ContactBottomSheetComponent>,
+    private deviceService: DeviceDetectorService
   ) {}
 
   public openMail(): void {
@@ -17,7 +19,15 @@ export class ContactBottomSheetComponent {
   }
 
   public openPhone(): void {
-    window.open('tel:+353 85 137 4535', '_system');
+    const deviceInfo = this.deviceService.getDeviceInfo();
+
+    // Check if the device is mobile using the device type property
+    const isMobile = deviceInfo.deviceType === 'smart-phone';
+
+    // If the device is mobile, open the tel link using window.open with _system parameter
+    if (isMobile) {
+      window.open(`tel:${this.phoneNumber}`, '_system');
+    }
   }
 
   public closeBottomSheet(): void {
