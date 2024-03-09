b09563eaa97f225be381095ab7ef835bccc314c8
Kevin O Keeffe <kevokeeffe@gmail.com>
2024-03-09 15:27:28 +0000

    Update

--- src/app/home/home/home.component.ts
+++ src/app/home/home/home.component.ts
@@ -1,8 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class HomeComponent {}
+export class HomeComponent {
+  isDesktop: boolean = true; // Assuming desktop view initially
+
+  constructor() {
+    this.checkScreenSize();
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize() {
+    this.checkScreenSize();
+  }
+
+  checkScreenSize() {
+    this.isDesktop = window.innerWidth > 500;
+  }
+}
