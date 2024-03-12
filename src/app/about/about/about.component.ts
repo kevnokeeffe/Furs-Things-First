eb48dcf6e9fde64983c6985774966e1ed1480dc4
Kevin O Keeffe <kevokeeffe@gmail.com>
2024-03-12 17:10:47 +0000

    Update to ui, desktop to mobile listener and ngIf to display

--- src/app/about/about/about.component.ts
+++ src/app/about/about/about.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrl: './about.component.scss'
+  styleUrl: './about.component.scss',
 })
 export class AboutComponent {
+  isDesktop: boolean = true; // Assuming desktop view initially
 
+  constructor() {
+    this.checkScreenSize();
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize() {
+    this.checkScreenSize();
+  }
+
+  checkScreenSize() {
+    this.isDesktop = window.innerWidth > 800;
+  }
 }
